dd3364dfa908eda171bb1cb0f614322458ff917e
Ale <alessandro.casabona.94@gmail.com>
2023-07-24 15:27:42 +0200

    fixes

--- router/router.go
+++ router/router.go
@@ -10,7 +10,7 @@ var mainRoute = "/api"
 
 func SetupRouter(db *gorm.DB) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("api/books", controller.GetAllBooks(db))
+	mux.HandleFunc(mainRoute+"/books", controller.GetAllBooks(db))
 	mux.HandleFunc(mainRoute+"/books/{id}", controller.GetBook(db))
 	mux.HandleFunc(mainRoute+"/upload", controller.UploadBook(db))
 	return mux
